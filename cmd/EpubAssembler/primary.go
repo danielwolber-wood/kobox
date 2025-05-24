b78173bc784134f3132f0f6f9eddfa4f5bf36741
danielwolber-wood <daniel@wolber-wood.com>
2025-05-24 09:39:02 -0500

    Finished Upload function

--- cmd/EpubAssembler/primary.go
+++ cmd/EpubAssembler/primary.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -39,4 +41,27 @@ func Generate(rr ReadabilityObject) (Epub, error) {
 }
 
 // Upload accepts an UploadObject and returns nothing
-func Upload(UploadObject any) {}
+func Upload(uploadObject UploadObject) error {
+	req, err := http.NewRequest("POST", "https://content.dropboxapi.com/2/files/upload", bytes.NewReader(uploadObject.Data))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/octet-stream")
+	req.Header.Set("Dropbox-API-Arg", fmt.Sprintf("{\"path\": \"%s\"}", uploadObject.DestinationPath))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("status: %v\n", resp.Status)
+	fmt.Printf("body: %v\n", body)
+	return nil
+}
