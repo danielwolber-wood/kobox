db4de620ab06e0763750ac262be58b864e39155b
danielwolber-wood <daniel@wolber-wood.com>
2025-05-24 05:42:24 -0500

    Refactored Server object to its own file

--- main.go
+++ main.go
@@ -4,46 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 )
-
-type Server struct {
-	readabilityParser *ReadabilityParser
-	opml              *OPML
-	opmlFilename      string
-}
-
-func newServer() (*Server, error) {
-	appdata, err := GetAppDataDir("kobox-mono")
-	if err != nil {
-		return nil, err
-	}
-	opmlFilename := filepath.Join(appdata, "opml.xml")
-	var opml OPML
-	if !FileExists(opmlFilename) {
-		_, err = os.Create(opmlFilename)
-		if err != nil {
-			return nil, err
-		}
-		opml = NewOPML()
-		opml.Save(opmlFilename)
-	} else {
-		opml, err = ParseOPMLFile(opmlFilename)
-		if err != nil {
-			return nil, err
-		}
-	}
-	parser, err := NewReadabilityParser()
-	if err != nil {
-		return nil, err
-	}
-	return &Server{
-		readabilityParser: parser,
-		opml:              &opml,
-		opmlFilename:      opmlFilename,
-	}, nil
-}
 
 func main() {
 	s, err := newServer()
