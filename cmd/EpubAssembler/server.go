45cb5cbd0775366a6d76597b194f17fe3f3c980e
danielwolber-wood <daniel@wolber-wood.com>
2025-05-24 08:18:05 -0500

    Added apiKey functionality

--- cmd/EpubAssembler/server.go
+++ cmd/EpubAssembler/server.go
@@ -1,15 +1,26 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 type Server struct {
 	readabilityParser *ReadabilityParser
+	dropboxApiKey     string
 }
 
 func newServer() (*Server, error) {
+	apiKey := os.Getenv("DROPBOX_API_KEY")
+	if apiKey == "" {
+		log.Fatal("DROPBOX_API_KEY not set")
+	}
 	parser, err := NewReadabilityParser()
 	if err != nil {
 		return nil, err
 	}
 	return &Server{
 		readabilityParser: parser,
+		dropboxApiKey:     apiKey,
 	}, nil
 }
